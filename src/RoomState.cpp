be8e2209be4547a07cad171b344354f1401931f3
Nunes <nunesgrf@gmail.com>
2019-07-09 12:03:54 -0300

    merge - conflito

--- src/RoomState.cpp
+++ src/RoomState.cpp
@@ -120,21 +120,12 @@ void RoomState::LoadAssets() {
   objects.push_back(playerGo);
   GameData::PlayerGameObject = playerGo;
 
-<<<<<<< HEAD
   std::cout << objects.size() << std::endl;
   // GameObject* terryGo = new GameObject(5);
   // Terry* terry = new Terry(
   //     *terryGo, currentTileMap->GetInitialPosition() + Vec2(-18, -18));
   // terryGo->AddComponent(terry);
   // objects.emplace_back(terryGo);
-=======
-  GameObject* antagonist_go = new GameObject(5);
-  std::vector<Vec2> points = {{68, 199},  {88, 132},  {110, 66},
-                              {199, 106}, {332, 132}, {326, 195}};
-  Antagonist* antagonist = new Antagonist(*antagonist_go, points);
-  antagonist_go->AddComponent(antagonist);
-  objects.emplace_back(antagonist_go);
->>>>>>> refact/terry-walk
 
   // GameObject* antagonist_go = new GameObject(5);
   // Antagonist* antagonist =
